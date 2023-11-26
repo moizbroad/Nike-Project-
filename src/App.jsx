6e843025f95fe88b5b11bb0083fe13cd01cd020a
moizbroad <moizbroadthinker543@gmail.com>
2023-11-26 19:22:49 +0500

    update folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import Forgot from './Pages/Forgot'
-import Main from './Pages/Main'
-import Signup from './Pages/Signup'
-import Reset from './Pages/Reset'
-import Inner from './Pages/Inner'
-// import login from './Pages/login'
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import { Navigate, useNavigate } from 'react-router';
-
-
+import Hero from './Section/Hero'
+import Footer from './Section/Footer'
+import CustomerReviews from './Section/CustomerReviews'
+import Services from './Section/Services'
+import SpecialOffer from './Section/SpecialOffer'
+import Subscrier  from './Section/Subscriber'
+import PopularProducts from './Section/PopularProducts'
 function App() {
-  // const [count, setCount] = useState(0)
+
 
   return (
-  <BrowserRouter>
-  <Routes>
-  <Route path="/" element={<Main />} exact />
-  <Route path="/inner" element={<Inner />} exact />
+  <main className='relative'> 
+    nav 
 
-  <Route path="/signup" element={<Signup />} exact />
-  <Route path="/forgot" element={<Forgot />} exact />
-  <Route path="/reset" element={<Reset />} exact />
+    <section className='xl:padding-1 wide:padding-r padding-b'>
+       
+    </section>
 
-  </Routes>
+    <section className='padding'> popularProducts </section>
+    <section className='padding'> Superquality    </section>
+    <section className='padding-x py-10'> Services        </section>
+    <section className='padding bg-pale-blue'> customer reviews   </section>
+    <section className='padding-x sm:py-32 py-16 w-full '> Subscriber   </section>
+    <section className=' bg-black padding-x padding-t pb-8  '> Footer   </section>
   
-  </BrowserRouter>
+  </main>
 
-
-
-   
- 
-   
-    
   )
 }
 
---
+++ src/Section/SuperQuality.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+const SuperQuality = () => {
+  return (
+    <div>SuperQuality</div>
+  )
+}
+
+export default SuperQuality
+
+
+
